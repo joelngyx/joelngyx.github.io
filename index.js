9e01ed011b6a55ff6c26dea81b73cda4b1dd7540
joelngyx <joelngyx@gmail.com>
2023-01-06 19:33:04 +0800

    Fixed body scrolling when Dialog is open

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function openModal (elementId) {
   var el = document.getElementById(elementId);
-  // TODO: Prevent close button from being auto-focused
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${window.scrollY}px`;
+
   el.showModal();
 }
 
 function closeModal (elementId) {
   var el = document.getElementById(elementId);
 
-  // if (elementId === 'school-project-cg2271') {
-  //   var vidEl = document.getElementById('cg2271vid');
-  //   vidEl.pause();
-  // }
-  
+  document.body.style.position = '';
+  document.body.style.top = `-${window.scrollY}px`;
+    
   el.close();
 }
 
