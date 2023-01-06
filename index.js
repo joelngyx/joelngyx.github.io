da48fb816a3a9ad00d7a5a4b7eb25f6b0b7582a6
joelngyx <joelngyx@gmail.com>
2023-01-06 18:46:28 +0800

    Updated site with details and other functionalities.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function openModal (elementId) {
   var el = document.getElementById(elementId);
-  console.log(el);
+  // TODO: Prevent close button from being auto-focused
   el.showModal();
 }
 
 function closeModal (elementId) {
   var el = document.getElementById(elementId);
+
+  if (elementId === 'school-project-cg2271') {
+    var vidEl = document.getElementById('cg2271vid');
+    vidEl.pause();
+  }
+  
   el.close();
+}
+
+function toggleVideo (elementId) {
+  var el = document.getElementById(elementId);
+
+  if (el.paused) {
+    el.play();
+  } else {
+    el.pause();
+  }
 }
